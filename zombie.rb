ea3bfbc6e95f446b71feb7b81f3b8432ec360f32
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-25 16:05:20 -0400

    Create Zombie class with class variables

---
+++ zombie.rb
@@ -0,0 +1,12 @@
+class Zombie
+  @@horde = []
+  @@plague_level = 10
+
+  #following variables do not change in value
+  @@max_speed = 5
+  @@max_strength = 8
+  @@default_speed = 1
+  @@default_strength = 3
+
+
+end

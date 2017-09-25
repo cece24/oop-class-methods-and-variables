f99109f2a3428b0ade7d10830e396084d6e0d594
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-25 16:10:25 -0400

    Add instance method initialize to set speed and strength of new zombie

--- zombie.rb
+++ zombie.rb
@@ -8,5 +8,22 @@ class Zombie
   @@default_speed = 1
   @@default_strength = 3
 
+  def initialize(speed, strength)
+    if speed > @@max_speed
+      @speed = @@max_speed
+    else
+      @speed = speed
+    end
+
+    if strength > @@max_strength
+      @strength = @@max_strength
+    else
+      @strength = strength
+    end
+  end
 
 end
+
+me_so_dead = Zombie.new(3, 6)
+
+puts me_so_dead.inspect

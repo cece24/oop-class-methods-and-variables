422058650810a376cefb54216a7d9a78ec573843
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-25 14:10:04 -0400

    Define class method to create new bank accounts and add to class accounts array

--- bankaccount.rb
+++ bankaccount.rb
@@ -2,6 +2,12 @@ class BankAccount
   @@interest_rate = 0.0
   @@accounts = []
 
+  def self.create
+    new_account = BankAccount.new
+    @@accounts << new_account
+    return new_account
+  end
+
   def initialize
     @balance = 0
 
@@ -25,16 +31,18 @@ class BankAccount
 
 end
 
-my_account = BankAccount.new
+puts BankAccount.create.inspect
 
-puts "Your account balance is #{my_account.balance}"
-
-my_account.balance = 100
-
-puts "Your account balance is now #{my_account.balance}"
-
-my_account.deposit(500)
-puts "Your account balance is now #{my_account.balance}"
-
-my_account.withdraw(250)
-puts "Your account balance is now #{my_account.balance}"
+# my_account = BankAccount.new
+#
+# puts "Your account balance is #{my_account.balance}"
+#
+# my_account.balance = 100
+#
+# puts "Your account balance is now #{my_account.balance}"
+#
+# my_account.deposit(500)
+# puts "Your account balance is now #{my_account.balance}"
+#
+# my_account.withdraw(250)
+# puts "Your account balance is now #{my_account.balance}"

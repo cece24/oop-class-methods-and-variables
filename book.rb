dd05cbbeefc736f28967a7d2ff3007797b658a19
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-25 14:41:33 -0400

    Add class methods create, available and borrowed

--- book.rb
+++ book.rb
@@ -8,5 +8,23 @@ class Book
     @isbn = isbn
   end
 
-  
+  def self.create(title, author, isbn)
+    new_book = Book.new(title, author, isbn)
+    @@on_shelf << new_book
+    return new_book
+  end
+
+  def self.available
+    @@on_shelf
+  end
+
+  def self.borrowed
+    @@on_loan
+  end
 end
+
+new_book = Book.create("Booping", "Cece", "2424")
+puts new_book.inspect
+
+puts Book.available.inspect
+puts Book.borrowed.inspect

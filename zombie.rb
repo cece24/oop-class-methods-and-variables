e58b29b30d80cc73ddf5395eb62e60152fde1ad4
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-25 16:22:22 -0400

    Add class spawn method to generate random number of zombies, based on plague level

--- zombie.rb
+++ zombie.rb
@@ -22,8 +22,22 @@ class Zombie
     end
   end
 
+  def self.all
+    @@horde
+  end
+
+  def self.spawn
+    number_of_zombies = rand(@@plague_level)
+    puts "About to spawn #{number_of_zombies} zombies..."
+    number_of_zombies.times do |zombie|
+      @@horde << Zombie.new(rand( @@max_speed ), rand( @@max_strength ))
+    end
+  end
 end
 
 me_so_dead = Zombie.new(3, 6)
 
 puts me_so_dead.inspect
+
+Zombie.spawn
+puts Zombie.all.inspect

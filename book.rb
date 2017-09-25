35c71cc5c15150df6b5f523de5ea06621e69953f
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-25 15:52:09 -0400

    Add and test class method to check for overdue books

--- book.rb
+++ book.rb
@@ -55,6 +55,10 @@ class Book
     @due_date
   end
 
+  def due_date=(date)
+    @due_date = date
+  end
+
   def return_to_library
     if self.lent_out?
       @due_date = nil
@@ -65,6 +69,12 @@ class Book
       puts "The book is not lent out, no return required."
     end
   end
+
+  def self.overdue
+    @@on_loan.select do |book|
+      book.due_date < Time.now
+    end
+  end
 end
 
 new_book = Book.create("Booping", "Cece", "2424")
@@ -72,6 +82,9 @@ puts new_book.inspect
 
 new_book2 = Book.create("Another One", "DJ Khaled", "2425")
 new_book3 = Book.create("Meows", "Professor Meow", "2426")
+overdue_book = Book.create("Best Book Ever", "Dinobot", "2464")
+overdue_book.borrow
+overdue_book.due_date = Time.now - 60
 
 puts Book.available.inspect
 puts Book.borrowed.inspect
@@ -90,3 +103,5 @@ puts "#{new_book.title} is lent out: #{new_book.lent_out?} and is due #{new_book
 new_book3.return_to_library
 new_book.return_to_library
 puts "#{new_book.title} is lent out: #{new_book.lent_out?} and is due #{new_book.due_date}"
+
+puts "List of overdue books: #{Book.overdue}"

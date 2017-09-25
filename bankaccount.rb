c98271f8aa40876848d80808f4a17bbf22363dd1
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-25 14:06:58 -0400

    Define instance methods for withdraw and deposit

--- bankaccount.rb
+++ bankaccount.rb
@@ -14,6 +14,15 @@ class BankAccount
   def balance=(new_balance)
     @balance = new_balance
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+
 end
 
 my_account = BankAccount.new
@@ -22,4 +31,10 @@ puts "Your account balance is #{my_account.balance}"
 
 my_account.balance = 100
 
+puts "Your account balance is now #{my_account.balance}"
+
+my_account.deposit(500)
+puts "Your account balance is now #{my_account.balance}"
+
+my_account.withdraw(250)
 puts "Your account balance is now #{my_account.balance}"

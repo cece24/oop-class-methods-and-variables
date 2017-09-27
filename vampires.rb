08e87fe490699ff88340a6d9370d38c04163767c
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-26 22:53:06 -0400

    Add drink blood and go home instance methods

--- vampires.rb
+++ vampires.rb
@@ -11,6 +11,14 @@ class Vampire
   def self.create(name, age)
     @@coven << Vampire.new(name, age)
   end
+
+  def drink_blood
+    @drank_blood_today = true
+  end
+
+  def go_home
+    @in_coffin = true
+  end
 end
 
 dracula = Vampire.create("Dracula", 450)

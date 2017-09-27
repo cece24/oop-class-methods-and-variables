628206b7e51708ae9d456e1670b533d511001af8
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-26 23:02:04 -0400

    Add class method for sunset to set each vampire in coven in_coffin to false and drank_blood_today to false

--- vampires.rb
+++ vampires.rb
@@ -8,6 +8,18 @@ class Vampire
     @drank_blood_today = false
   end
 
+  def self.coven_status
+    @@coven
+  end
+
+  def in_coffin=(in_coffin)
+    @in_coffin = in_coffin
+  end
+
+  def drank_blood_today=(drank_blood_today)
+    @drank_blood_today = drank_blood_today
+  end
+
   def self.create(name, age)
     @@coven << Vampire.new(name, age)
   end
@@ -19,8 +31,25 @@ class Vampire
   def go_home
     @in_coffin = true
   end
+
+  def self.sunrise
+
+
+  end
+
+  def self.sunset
+    @@coven.each do |vampire|
+      vampire.in_coffin = false
+      vampire.drank_blood_today = false
+    end
+  end
 end
 
 dracula = Vampire.create("Dracula", 450)
+batboy = Vampire.create("Batsy", 230)
 
 puts dracula.inspect
+
+Vampire.sunset
+
+puts Vampire.coven_status.inspect
